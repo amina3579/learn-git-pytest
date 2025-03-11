e9bf28ffbb8a635a7ffb26de41ca8c4e30f29238
amina3579 <aminadione2005@gmail.com>
2025-03-11 13:49:18 +0000

    Amina_Update string_utils.py

--- exercise2/string_utils.py
+++ exercise2/string_utils.py
@@ -12,6 +12,9 @@ def reverse_string(s: str) -> str:
         The reversed string
     """
     # TODO: Implement this function
+    return ( s[::-1])
+    pass
+reverse_string("abracadabra")
     pass
 
 
@@ -27,6 +30,15 @@ def count_vowels(s: str) -> int:
         The number of vowels in the string
     """
     # TODO: Implement this function
+    L=list(s)
+    Vowels=['a', 'e', 'i', 'o', 'u']
+    count = 0
+    for i in range (len(L)) :
+        if L[i].lower() in Vowels:
+            count+=1
+    return count
+count_vowels('flagadouille')
+
     pass
 
 
@@ -43,7 +55,10 @@ def is_palindrome(s: str) -> bool:
         True if the string is a palindrome, False otherwise
     """
     # TODO: Implement this function
+    return ( s==s[::-1])
     pass
+is_palindrome("madam")
+
 
 
 def capitalize_words(s: str) -> str:
@@ -57,4 +72,9 @@ def capitalize_words(s: str) -> str:
         The input string with the first letter of each word capitalized
     """
     # TODO: Implement this function
+    if not s:  # handle empty string
+        return ""
+    return s[0].upper() + s[1:].lower()
+    pass
+capitalize_words("marmite")
     pass
